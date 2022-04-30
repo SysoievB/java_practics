c56a6070f2f4f4b4e5e935daaa10c0ba6fb24ccb
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-04-30 13:24:56 +0200

    added abstract

--- src/main/java/tasks/abstract_class/AbstractClass.java
+++ src/main/java/tasks/abstract_class/AbstractClass.java
@@ -1,16 +1,36 @@
 package tasks.abstract_class;
 
 class AbstractClass {
+    private String name;
+    private int age;
 
-    public AbstractClass() {
+    public AbstractClass(String name, int age) {
         System.out.println("I am from abstract class");
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 
 class RealClass extends AbstractClass {
 
-    public RealClass() {
-        super();
+    public RealClass(String name, int age) {
+        super(name, age);
         System.out.println("I am from real class");
     }
 
@@ -26,7 +46,13 @@ class RealClass extends AbstractClass {
     }
 
     public static void main(String[] args) {
-        RealClass realClass = new RealClass();
+        RealClass realClass = new RealClass("real", 14);
+        System.out.println(realClass);
         realClass.foo();
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " " + super.getAge();
     }
 }
